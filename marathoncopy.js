4aaacc608c62461edf2d12b6e1e6bf3c895133dd
max <massimo.strambini@gmail.com>
2022-02-20 14:13:04 +0100

    2022-02-20, INIT, studio

---
+++ marathoncopy.js
@@ -0,0 +1,20 @@
+module.exports = function(RED) {
+    function MarathonCopyNode(config) {
+        RED.nodes.createNode(this,config);
+        this.field = config.field || "payload";
+        this.template = config.template;
+        this.fieldType = config.fieldType || "msg";
+        var node = this;
+        node.on("input", function(msg) {
+            try {
+                msg.payload = msg.payload;
+                node.send(msg);
+            }
+            catch(e) {
+                node.error(e.message);
+            }
+        });
+    }
+    RED.nodes.registerType("borneo-marathon",MarathonCopyNode);
+    RED.library.register("borneo-marathon");
+}
